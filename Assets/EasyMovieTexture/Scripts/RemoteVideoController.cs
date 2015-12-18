b3a1d2d9c620d2d659f587eb93412b5d884c5a11
MartinDrost <info@martindrost.nl>
2015-12-18 12:54:42 +0100

    Working copy

---
+++ Assets/EasyMovieTexture/Scripts/RemoteVideoController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Threading;
+
+public class RemoteVideoController : MonoBehaviour {
+
+    public MediaPlayerCtrl scrMedia;
+    
+    // Use this for initialization
+    void Start()
+    {
+        /*
+        scrMedia.Load("http://martindrost.nl/rock360.mp4");
+        scrMedia.Play();
+        scrMedia.Stop();
+        scrMedia.Pause();
+        scrMedia.UnLoad();
+        */
+    }
+
+    // Update is called once per frame
+    int count = 0;
+    void Update () {
+
+    }
+
+    void OnGUI()
+    {
+        GUI.Label(new Rect(new Vector2(0, 0), new Vector2(Screen.width, Screen.height)), count + "");
+    }
+}
