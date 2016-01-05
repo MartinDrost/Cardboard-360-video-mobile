99cf3764af165ef61676d2232e6f810bd27c7f3b
MFDemon <info@martindrost.nl>
2016-01-05 21:15:36 +0100

    Added JSON calls

--- Assets/EasyMovieTexture/Scripts/RemoteVideoController.cs
+++ Assets/EasyMovieTexture/Scripts/RemoteVideoController.cs
@@ -1,6 +1,10 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Threading;
+using System.Net;
+using JsonFx.Json;
+using System;
+using AssemblyCSharp;
 
 public class RemoteVideoController : MonoBehaviour {
 
@@ -9,6 +13,7 @@ public class RemoteVideoController : MonoBehaviour {
     // Use this for initialization
     void Start()
     {
+		fetchActionInterval ();
         /*
         scrMedia.Load("http://martindrost.nl/rock360.mp4");
         scrMedia.Play();
@@ -18,14 +23,85 @@ public class RemoteVideoController : MonoBehaviour {
         */
     }
 
+	int lastProcessedAction = 0;
+	void fetchActionInterval()
+	{
+		scrMedia.Load("http://martindrost.nl/rock360.mp4");
+		new Thread((ThreadStart)delegate()
+		{
+			while(true)
+			{
+					continue;
+					string data = new WebClient().DownloadString("http://martindrost.nl/vr/getActions?id=" + lastProcessedAction);
+					VRAction[] actions = JsonReader.Deserialize<VRAction[]>(data);
+					
+					foreach(VRAction action in actions)
+					{
+						Debug.Log("New action: " + action.action);
+						DateTime targetDate = Convert.ToDateTime(action.time);
+						DateTime currentDate = DateTime.Now;
+
+						int timeout = 0;
+						if(targetDate > currentDate)
+						{
+							TimeSpan timeSpan = targetDate - currentDate;
+							timeout = (int) timeSpan.TotalMilliseconds;
+						}
+
+						switch(action.action)
+						{
+							case "load":
+								new Thread((ThreadStart)delegate()
+								{
+									Thread.Sleep(timeout);
+									scrMedia.Load(action.details);
+								}).Start();
+								break;
+							case"play":
+								new Thread((ThreadStart)delegate()
+								{
+									Thread.Sleep(timeout);
+									scrMedia.Play();
+								}).Start();
+								break;
+							case"pause":
+								new Thread((ThreadStart)delegate()
+								{
+									Thread.Sleep(timeout);
+									scrMedia.Pause();
+								}).Start();
+								break;
+							case"stop":
+								new Thread((ThreadStart)delegate()
+								{
+									Thread.Sleep(timeout);
+									scrMedia.Stop();
+								});
+								break;
+							case "seek":
+								new Thread((ThreadStart)delegate()
+								{
+									Thread.Sleep(timeout);
+									scrMedia.SeekTo(Int32.Parse(action.details));
+								}).Start();
+								break;
+						}
+
+						lastProcessedAction = Int32.Parse(action.id);
+					}
+
+					Thread.Sleep(5000);
+			}
+		}).Start();
+	}
+
     // Update is called once per frame
-    int count = 0;
     void Update () {
 
     }
 
     void OnGUI()
     {
-        GUI.Label(new Rect(new Vector2(0, 0), new Vector2(Screen.width, Screen.height)), count + "");
-    }
+        GUI.Label(new Rect(new Vector2(0, 0), new Vector2(Screen.width, Screen.height)), " url's called");
+	}
 }
---
+++ Assets/EasyMovieTexture/Scripts/VRAction.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace AssemblyCSharp
+{
+	[System.Serializable]
+	public class VRAction {
+		public string id { get; set;}
+		public string action { get; set;}
+		public string details { get; set;}
+		public string time { get; set;}
+
+		public VRAction()
+		{
+		}
+	}
+}
+
